e09c9d1552ea6cb5f85966be526ac7703ed9720a
frank2016ma <31381431+frank2016ma@users.noreply.github.com>
2021-03-20 15:01:51 +0800

    Add files via upload

---
+++ listFiles.ps1
@@ -0,0 +1,11 @@
+﻿$sourceFolder = "F:\data"
+
+$dirs = Get-ChildItem -Path $sourceFolder -Directory -Recurse
+$logFile = "C:\Users\K20075306\Desktop\list.txt"
+foreach($dir in $dirs){
+    $dir.FullName
+    $files = Get-ChildItem -Path $dir.FullName -File -Recurse
+    foreach($file in $files){
+        $file.FullName | Out-File $logFile -Append
+    }
+}
---
+++ rename_files.ps1
@@ -0,0 +1,32 @@
+﻿# Get-ChildItem -Recurse | Rename-Item -NewName {'000528_' + $_.Name}
+# tree | Out-File "C:\Users\K20075306\Desktop\fk.txt"
+# Get-ChildItem -Recurse -File | Move-Item -Destination ""
+
+$imgs = Get-ChildItem -Path "D:\Notebooks"
+$i = 1
+foreach($img in $imgs){
+    $nn = ("Snip_" + $i.ToString() +".png")
+    Rename-Item -Path $img.FullName -NewName $nn
+    $i++
+}
+
+<#
+$files = Get-ChildItem -Path "E:\AA_OCR\dm" -Recurse
+foreach($child in $files){
+#     $child.Name
+#     $child.fullname
+#     $child | Get-Member
+    $child.Extension
+    $child.Exists
+    $child.GetType()
+}
+#>
+
+
+<#
+    System.IO.FileInfo
+    GetType()
+    Extension: 后缀名(.txt, .doc)
+    Name 有后缀名(.png, .pdf...), BaseName 没有后缀名
+    DirectoryName 是String型, Directory是对象
+#>
